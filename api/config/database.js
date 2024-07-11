5383e87dbe7e632796edf145e774fca909b3ac8b
robertGhandi <robertzerah@gmail.com>
2024-07-11 01:37:54 +0100

    updated database file

--- api/config/database.js
+++ api/config/database.js
@@ -1,18 +1,10 @@
 require("dotenv").config();
 const { Pool } = require("pg");
 
-//const pool = new Pool({
-// user: process.env.DB_USER,
-//host: process.env.DB_HOST,
-//database: process.env.DB_NAME,
-//password: process.env.DB_PASSWORD,
-//port: process.env.DB_PORT,
-//});
-
 const pool = new Pool({
 	connectionString: process.env.POSTGRES_URL,
 	ssl: {
-		rejectUnauthorized: false, // Adjust based on your Supabase configuration
+		rejectUnauthorized: false,
 	},
 });
 
